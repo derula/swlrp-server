ab20943e6412a523b7db7e8822874cc48c5c855a
derula <derula@uglyhorst.de>
2017-07-04 22:38:55 +0200

    Added database schema.

---
+++ schema/setup.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `characters` (
+    `id` INT(11) NOT NULL,
+    `nick` VARCHAR(40) NOT NULL,
+    `first` VARCHAR(255) NOT NULL,
+    `last` VARCHAR(255) NOT NULL,
+    `first_write` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE (`nick`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `properties` (
+    `id` INT(11) NOT NULL,
+    `name` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `character_properties` (
+    `character_id` INT(11) NOT NULL,
+    `property_id` INT(11) NOT NULL,
+    `value` VARCHAR(40) NOT NULL,
+    PRIMARY KEY (`character_id`, `property_id`),
+    KEY `value` (`value`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `character_texts` (
+    `character_id` INT(11) NOT NULL,
+    `property_id` INT(11) NOT NULL,
+    `text` TEXT NOT NULL,
+    PRIMARY KEY (`character_id`, `property_id`)
+) ENGINE=InnoDB;
