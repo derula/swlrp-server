98e50f827b5b76e174cf85ac484b4dc9934a72f7
derula <derula@uglyhorst.de>
2017-07-04 23:21:15 +0200

    Implemented profile load method.

--- src/Models/Profile.php
+++ src/Models/Profile.php
@@ -4,8 +4,24 @@ namespace Incertitu\SWLRP\Views;
 use Incertitu\SWLRP\Model;
 
 class Profile extends Model {
-    public function load(string $name) {
-        // Implementation
+    const Q_LOAD = <<<'QUERY'
+SELECT
+    CONCAT(`first`, ' "', `nick`, '" ', `last`) AS `name`,
+    COALESCE(`p2`.`name`, `p1`.`name`) AS `key`,
+    COALESCE(`text`, `value`) AS `value`
+FROM `characters` AS `c`
+    LEFT JOIN `character_properties` AS `cp` ON `c`.`id` = `cp`.`character_id`
+    LEFT JOIN `properties` AS `p1` ON `cp`.`property_id` = `p1`.`id`
+    LEFT JOIN `character_texts` AS `ct` ON `c`.`id` = `ct`.`character_id`
+    LEFT JOIN `properties` AS `p2` ON `ct`.`property_id` = `p2`.`id`
+WHERE `nick` = :nick;
+QUERY;
+    public function load(string $name): ?array {
+        $statement = $this->getConnection()->prepare(self::Q_LOAD);
+        if ($statement->execute([':nick' => $name])) {
+            return $statement->fetchAll();
+        }
+        return null;
     }
     public function save(string $name) {
         // Implementation
