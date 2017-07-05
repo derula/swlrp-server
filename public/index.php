edd699c6415e2b71a10bee0895054b85d869922a
derula <derula@uglyhorst.de>
2017-07-05 13:07:40 +0200

    Use combined URI parts + GET/POST as data in IOComponents.

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 use Incertitude\SWLRP\Application;
 use Incertitude\SWLRP\Exceptions\HttpError;
 require dirname(__DIR__) . '/vendor/autoload.php';
-$get = explode('/', substr($_SERVER['REQUEST_URI'], 1));
-$route = array_shift($get);
-$app = new Application(dirname(__DIR__), $get, $_POST);
+$uriParts = explode('/', substr($_SERVER['REQUEST_URI'], 1));
+$route = array_shift($uriParts);
+$app = new Application(dirname(__DIR__), $uriParts + $_GET, $uriParts + $_POST);
 try {
     switch($route) {
         case 'edit':
