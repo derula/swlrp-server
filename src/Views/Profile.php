98943da89d8aacfdca88abe8d3598b29cceb47c4
derula <derula@uglyhorst.de>
2017-07-05 03:47:19 +0200

    Prepare profile data for safe use in template.

--- src/Views/Profile.php
+++ src/Views/Profile.php
@@ -6,6 +6,11 @@ use Incertitude\SWLRP\View;
 use Incertitude\SWLRP\Exceptions\ProfileNotFound;
 
 abstract class Profile extends View {
+    const PROP_DEFAULTS = [
+        'name' => '',
+        'autocomplete' => false,
+        'constraint' => 'none',
+    ];
     /** @var array */
     private $profile;
     protected function getTitle(): string {
@@ -14,16 +19,33 @@ abstract class Profile extends View {
     protected function getContent(): string {
         return $this->renderTemplate('profile', $this->getProfile());
     }
+    protected function getRequestedName(): string {
+        return ucfirst($this->getData(0));
+    }
     protected function getProfile(): array {
         if (!isset($this->profile)) {
-            $this->profile = $this->getModel()->load($this->getRequestedName());
+            $this->profile = [];
+            $data = $this->getModel()->load($this->getRequestedName());
+            if (!empty($data)) {
+                $this->loadProfileData($data);
+            }
         }
         if (empty($this->profile)) {
             throw new ProfileNotFound();
         }
         return $this->profile;
     }
-    protected function getRequestedName(): string {
-        return ucfirst($this->getData(0));
+    protected function loadProfileData(array $data) {
+        $this->profile['name'] = $data['name'];
+        $this->profile['structure'] = $this->getModel()->getMetadata();
+        foreach ($this->profile['structure'] as &$profilePage) {
+            foreach (['properties', 'texts'] as $key) {
+                foreach ($profilePage[$key] as &$prop) {
+                    $prop += self::PROP_DEFAULTS;
+                    $prop += ['title' => ucfirst($prop['name'])];
+                    $prop['value'] = $data['propertier'][$prop['name']] ?? '';
+                }
+            }
+        }
     }
 }
