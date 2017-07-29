93b036b7c97791bf08fcd6887032045b1f5cec96
derula <derula@uglyhorst.de>
2017-07-29 15:45:39 +0200

    Made the profile error forwardable.

--- src/Views/Editor.php
+++ src/Views/Editor.php
@@ -3,11 +3,19 @@
 namespace Incertitude\SWLRP\Views;
 
 use Incertitude\SWLRP\Application;
+use Incertitude\SWLRP\Forwardable;
 
-class Editor extends Profile {
+class Editor extends Profile implements Forwardable {
     public function __construct(array $data, Application $application) {
         parent::__construct($data, $application);
         $this->setRequestedId($application->getSession()->getCharacterId());
+    }
+    public function getRequestString(): string {
+        $id = $this->getData(0);
+        if (!empty($id)) {
+            return $id . '?' . http_build_query($this->getNameData());
+        }
+        return '';
     }
     protected function getProfile(): array {
         return ['editMode' => true] + parent::getProfile();
