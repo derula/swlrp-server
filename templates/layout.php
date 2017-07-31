23dbcf73862f8fb68f8adf25a254fe3c094988e4
derula <derula@uglyhorst.de>
2017-07-31 20:43:10 +0200

    Switched layout.php to use local jQuery and SCEditor.

--- templates/layout.php
+++ templates/layout.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <title><?=$title?></title>
-<script src="//code.jquery.com/jquery-3.2.1.min.js"
-        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-        crossorigin="anonymous"></script>
-<script src="//code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-        integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
-        crossorigin="anonymous"></script>
-<script src="//cloud.tinymce.com/stable/tinymce.min.js?apiKey=<?=$mceApiKey?>"></script>
+<script src="/components/jquery/jquery.min.js"></script>
+<script src="/components/jquery-ui/jquery-ui.min.js"></script>
+<script src="/components/sceditor/minified/jquery.sceditor.min.js"></script>
 <script src="/assets/script<?=$useCompatJs?'.compat':''?>.js"></script>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/dark-hive/jquery-ui.css" />
+<link rel="stylesheet" href="/components/jquery-ui/themes/dark-hive/jquery-ui.css" />
+<link rel="stylesheet" href="/components/sceditor/minified/themes/modern.min.css" />
 <link rel="stylesheet" href="/assets/style.css" />
 <?=$content?>
