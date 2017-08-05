d0d70a16c83fb834b7ecf53f122a940fa09f161b
derula <derula@uglyhorst.de>
2017-08-05 14:43:02 +0200

    Changed to SCEditor monochromatic theme.

--- templates/layout.php
+++ templates/layout.php
@@ -5,6 +5,6 @@
 <script src="/components/sceditor/minified/jquery.sceditor.min.js"></script>
 <script src="/assets/script<?=$useCompatJs?'.compat':''?>.js"></script>
 <link rel="stylesheet" href="/components/jquery-ui/themes/dark-hive/jquery-ui.css" />
-<link rel="stylesheet" href="/components/sceditor/minified/themes/modern.min.css" />
+<link rel="stylesheet" href="/components/sceditor/minified/themes/monocons.min.css" />
 <link rel="stylesheet" href="/assets/style.css" />
 <?=$content?>
