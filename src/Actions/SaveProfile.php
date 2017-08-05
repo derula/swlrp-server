a3ac977a52ece7bb9fd637d25dd7739655a9cd8c
derula <derula@uglyhorst.de>
2017-08-06 00:03:25 +0200

    Truncate and "purify" user input.

--- src/Actions/SaveProfile.php
+++ src/Actions/SaveProfile.php
@@ -9,11 +9,29 @@ use Incertitude\SWLRP\Models\Profile;
  * @method Profile getModel()
  */
 class SaveProfile extends Action {
+    const MAX_LENGTH = ['properties' => 40, 'texts' => 40000];
     public function execute() {
-        $id = $this->getSession()->getCharacterId();
-        $this->getModel()->saveProperties(
-            $id,
-            $this->getData()
+        $data = $this->getData();
+        $saveData = [];
+        $hpConfig = \HTMLPurifier_Config::createDefault();
+        $hpConfig->set('HTML.Doctype', 'HTML 4.01 Transitional');
+        $attrDef = $hpConfig->getHTMLDefinition(true);
+        $attrDef->addAttribute(
+            'p', 'style', new \HTMLPurifier_AttrDef_Enum(['direction: rtl;'])
         );
+        $hp = new \HTMLPurifier($hpConfig);
+        foreach ($this->iterateMetaData() as $type => $prop) {
+            $name = $prop['name'];
+            if (!isset($data[$name])) {
+                continue;
+            }
+            $value = mb_substr($data[$name], 0, self::MAX_LENGTH[$type] ?? 20);
+            if ('texts' === $type) {
+                $value = $hp->purify($value);
+            }
+            $saveData[$name] = $value;
+        }
+        $id = $this->getSession()->getCharacterId();
+        $this->getModel()->saveProperties($id, $saveData);
     }
 }
