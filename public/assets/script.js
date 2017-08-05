7eaf3edbdd06ed768373ca294c0b4e82c667c722
derula <derula@uglyhorst.de>
2017-08-05 03:37:58 +0200

    Use SCEditor in script.

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,9 +1,8 @@
 $(() => {
-    const maxTextLen = 20000;
     const wrap = (button) => {
         $('body').wrapInner('<form method="POST">');
         $('.editable').each((i, e) => {
-            const prop = $(e).data('prop'), isText = $(e).is('.text');
+            const prop = $(e).data('prop'), isText = $(e).is('._text');
             const field = $(isText ? '<textarea>' : '<input>').attr({
                 id: 'property-' + prop.name,
                 class: 'ui-widget-content ui-corner-all',
@@ -19,8 +18,11 @@ $(() => {
             field.val($(e).html());
             $(e).html(field);
         });
-        $('textarea', '.editable.text').each((i, e) => {
-            tinymce.EditorManager.execCommand('mceAddEditor', false, $(e).attr('id'));
+        $('textarea', '.editable._text').sceditor({
+            style: '/components/sceditor/minified/jquery.sceditor.default.min.css',
+            emoticonsRoot: '/components/sceditor/',
+            width: '100%',
+            resizeEnabled: false,
         });
         button.button('option', 'label', 'Preview')
             .after($('<button>').button({label: 'Change password'}).click(changePW))
@@ -28,15 +30,15 @@ $(() => {
     };
     const unwrap = (button) => {
         $('form').children().unwrap();
-        $('textarea', '.editable.text').each((i, e) => {
-            const id = $(e).attr('id');
-            tinymce.EditorManager.execCommand('mceFocus', false, id);
-            tinymce.EditorManager.execCommand('mceRemoveEditor', true, id);
+        $('> textarea', '.editable._text').each((i, e) => {
+            const instance = $(e).sceditor('instance');
+            instance.updateOriginal();
+            instance.destroy();
         });
         $('.editable').each((i, e) => {
             const prop = $(e).data('prop');
             const field = $('input, textarea', e);
-            field.replaceWith(field.val());
+            field.replaceWith($(field.val().substr(0, 20000)));
         });
         button.button('option', 'label', 'Change profile').nextAll().remove();
     };
@@ -47,9 +49,6 @@ $(() => {
     const savePW = (e) => {
         e.preventDefault();
         $('#changePW').find('form').submit();
-    };
-    const getMceLength = () => {
-        return $('body', tinymce.get(tinymce.activeEditor.id).contentDocument).html().length;
     };
     $('#edit').button({label: 'Change profile'}).click((e) => {
         e.preventDefault();
@@ -74,38 +73,6 @@ $(() => {
         if (o.length > 1) {
             const v = o.first().val() !== o.last().val() ? 'Passwords must match.' : '';
             o.last()[0].setCustomValidity(v);
-        }
-    });
-    tinymce.init({
-        selector:'none',
-        menubar: false,
-        plugins: 'paste',
-        setup: (ed) => {
-            const allowedKeys = [8, 19, 20, 27, 33, 34, 35, 36, 37, 38, 39, 40, 45, 46, 91, 92, 93, 144, 145];
-            ed.showWarning = () => {
-                $('#maxLength').dialog('open').dialog('option', 'close', () => {
-                    ed.focus();
-                });
-                return false;
-            };
-            ed.on('keydown', (e) => {
-                if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey || allowedKeys.indexOf(e.keyCode) != -1) {
-                    return true;
-                }
-                if (getMceLength() + 1 > maxTextLen) {
-                    ed.on('keyup', ed.showWarning);
-                    e.preventDefault();
-                    e.stopPropagation();
-                    return false;
-                }
-                return true;
-            });
-        },
-        paste_preprocess: (plugin, args) => {
-            if (getMceLength() + args.content.length > maxTextLen) {
-                tinymce.activeEditor.showWarning();
-                args.content = '';
-            }
         }
     });
 });
